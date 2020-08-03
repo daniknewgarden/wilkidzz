61710ceb5ffb62b6b175584e009a411051880862
Danik <daniknewgarden@gmail.com>
2020-08-03 19:00:36 +0300

    Returned normal svg copy

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './styles/app.scss';
+import './styles/app.css';
 import './styles/test.scss';
 import "./js/test";
--- webpack.config.js
+++ webpack.config.js
@@ -1,92 +1,29 @@
 const isDevelopment = process.env.NODE_ENV === 'development';
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+
+//Plugins
+const path = require('path'); //Path from this folder (not root C:\\) plugin
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); //Css separate file plugin
+const CopyPlugin = require('copy-webpack-plugin'); //Copy static files plugin
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // Require  html-webpack-plugin plugin
 
+//Modules
 module.exports = {
-    entry: __dirname + "/src/index.js", // webpack entry point. Module to start building dependency graph
-    output: {
-        path: __dirname + '/dist', // Folder to store generated bundle
-        filename: 'bundle.js' // public URL of the output directory when referenced in a browser
-    },
-    module: {
-        rules: [{
-            test: /\.js$/,
-            use: 'babel-loader',
-            exclude: [
-                /node_modules/
-            ]
-        },
-        {
-          test: /\.(jpe?g|png|gif|webp)$/i,
-          /* Exclude fonts while working with images, e.g. .svg can be both image or font. */
-          // exclude: path.resolve(__dirname, '../src/static/images'),
-          use: [{
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'images/'
-            }
-          }]
-        },
-        {
-          test: /\.svg$/,
-          oneOf: [{
-              exclude: path.resolve(__dirname, 'static/images/icons'),
-              use: [{
-                loader: 'svg-inline-loader',
-                options: {
-                  name: '[name].[ext]',
-                  outputPath: 'images/'
-                }
-              }]
-            },
-            {
-              include: path.resolve(__dirname, 'static/images/icons'),
-              use: [{
-                loader: 'file-loader',
-                options: {
-                  name: '[name].[ext]',
-                  outputPath: 'images/'
-                }
-              }]
-            },
-          ],
-        }, 
-        {
-        test: /\.module\.s(a|c)ss$/,
-        loader: [
-          isDevelopment ? 'style-loader' : MiniCssExtractPlugin.loader,
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: isDevelopment
-            }
-          },
-          {
-            loader: 'resolve-url-loader',
-            options: {
-              root: path.join(__dirname, 'src')
-            }
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-              sourceMap: isDevelopment
-            }
-          },
-          {
-            loader: 'postcss-loader',
-            options: {
-              sourceMap: isDevelopment,
-              config: { path: `./postcss.config.js` }
-            }
-          },
-          
+  entry: __dirname + "/src/index.js", // webpack entry point. Module to start building dependency graph
+  output: {
+    path: __dirname + '/dist', // Folder to store generated bundle
+    filename: 'bundle.js' // public URL of the output directory when referenced in a browser
+  },
+  module: {
+    rules: [
+      //javaScript
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: [
+          /node_modules/
         ]
       },
+      //Styles 
       {
         test: /\.s(a|c)ss$/,
         exclude: /\.module.(s(a|c)ss)$/,
@@ -111,32 +48,86 @@ module.exports = {
         ]
       },
       {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        // css
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              sourceMap: true,
+              config: {
+                path: `./postcss.config.js`
+              }
+            }
+          }
+        ]
+      },
+      //Images
+      {
+        test: /\.(jpe?g|png|gif|webp|svg)$/i,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'images/'
+          }
+        }]
+      },
+      // //Svg images and icons
+      // {
+      // {
+      //   test: /\.svg$/,
+      //   exclude: path.resolve(__dirname, 'static/images/icons'),
+      //   use: [{
+      //       loader: 'svg-inline-loader',
+      //       options: {
+      //         name: '[name].[ext]',
+      //         outputPath: 'images/'
+      //       }
+      //     },
+      //     {
+      //       loader: 'file-loader'
+      //     }
+      //   ]
+      // },
+      {
+        test: /\.woff(2)?$/,
         loader: 'file-loader',
         options: {
           name: '[name].[ext]'
         }
-      }]
-    },
-    resolve: {extensions: ['.js', '.jsx', '.scss']},
-    plugins: [ // Array of plugins to apply to build chunk
-        new HtmlWebpackPlugin({
-            template: __dirname + "/src/index.html",
-            inject: 'body'
-        }),
-        new MiniCssExtractPlugin({
-            filename: isDevelopment ? '[name].css' : '[name].css',
-            chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css' 
-        }),
-        new CopyPlugin({
-            patterns: [
-              { from: 'src/static/images', to: 'static/images' }
-            ],
-        }),
-    ],
-    devServer: { // configuration for webpack-dev-server
-        contentBase: './src', //source of static assets
-        port: 7700, // port to run dev-server
-    }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.scss']
+  },
+  plugins: [ // Array of plugins to apply to build chunk
+    new HtmlWebpackPlugin({
+      template: __dirname + "/src/index.html",
+      inject: 'body'
+    }),
+    new MiniCssExtractPlugin({
+      filename: isDevelopment ? '[name].css' : '[name].css',
+      chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
+    }),
+    // new CopyPlugin({
+    //   patterns: [{
+    //     from: 'src/static/images',
+    //     to: 'images'
+    //   }],
+    // }),
+  ],
+  devServer: { // configuration for webpack-dev-server
+    contentBase: './src', //source of static assets
+    port: 7700, // port to run dev-server
+  }
 };
-
